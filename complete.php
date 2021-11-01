cbb3a0f8446a7a2f371ea3e7c0762af5e7e0be55
c200021 <83998429+c200021@users.noreply.github.com>
2021-11-01 11:52:30 +0900

    Update complete.php

--- complete.php
+++ complete.php
@@ -19,7 +19,7 @@
     $stmt->bindValue(':gender', $gender, PDO::PARAM_INT);
     $stmt->execute(); 
     // [DB切断]
-    $ = null;
+    $dbh = null;
     }catch (PDOException $e){ 
         echo($e->getMessage()); 
         die(); 
